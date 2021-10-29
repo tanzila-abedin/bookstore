83d48a8d7eaf7e68fc7efc35bdd2ff0cbc9d5cf6
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-10-29 21:01:49 +0200

    Create simple html table to display information of a single book and use useSelector hook to connect state to component

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,5 +1,33 @@
-const BooksList = () => (
-  <h1>BooksList</h1>
-);
+import { nanoid } from 'nanoid';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import book from '../components/Book';
+
+const newBook = {
+  id: nanoid(),
+  title: '5 am morning club',
+  category: 'personal development',
+};
+
+const BooksList = () => {
+  const { books } = useSelector((state) => state.books);
+  const displayBook = book(newBook);
+  return (
+    <div className="booklist">
+      <table className="booklist-table">
+        <thead>
+          <tr className="booklist-row">
+            <th className="booklist-header">ID</th>
+            <th className="booklist-title">Title Of Book</th>
+            <th className="booklist-category">Category Of Book</th>
+          </tr>
+        </thead>
+        <tbody>
+          {displayBook}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default BooksList;
