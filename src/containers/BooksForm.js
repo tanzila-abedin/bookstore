8e700186449488a6041fd7ac5c5c241c092afd42
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-10-29 21:05:23 +0200

    Create simple book form with drop down categories  to  add new books

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,5 +1,19 @@
-const BooksForm = () => (
-  <h1>BooksForm</h1>
-);
+import Select from 'react-select';
 
+const BooksForm = () => {
+  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const option = categories.map((category) => ({
+    value: category,
+    label: category,
+  }));
+  return (
+    <div className="booklist">
+      <form>
+        <input type="text" id="title" name="title" placeholder="Book Title" />
+        <Select option={option} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 export default BooksForm;
