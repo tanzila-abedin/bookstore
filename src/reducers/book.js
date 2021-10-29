245d89c4ff4efa1709f2692111367a195440f11f
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-10-29 23:31:41 +0200

    Set initial state

--- src/reducers/book.js
+++ src/reducers/book.js
@@ -1,19 +1,25 @@
 // use combire reduce
 import { nanoid } from 'nanoid';
+import { CREATE_BOOK, REMOVE_BOOK } from '../actions';
 
-const initialState = [
-
-  { id: nanoid(), title: 'Rich Dad Poor Dad', category: 'Finance' },
-  { id: nanoid(), title: 'The Art of seduction', category: 'Non-fiction' },
-
-];
+const initialState = {
+  book: [
+    { id: nanoid(), title: 'Rich Dad Poor Dad', category: 'Finance' },
+    { id: nanoid(), title: 'The Art of seduction', category: 'Non-fiction' },
+  ],
+};
 
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
-    case '':
+    case CREATE_BOOK:
+
       return {
+        ...state,
+        newBook: action.payload,
 
       };
+    case REMOVE_BOOK:
+      return state.filter((book) => book.bookId !== action.payload.bookId);
     default:
       return state;
   }
