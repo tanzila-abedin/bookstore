ab414f1ad77a15328dcc61da58a995e07e310452
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-11-01 16:03:36 +0200

    Set inital state of the store on index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
+import { nanoid } from 'nanoid';
 import { createStore } from 'redux';
 import App from './components/App';
-import rootReducer from './reducers/index';
+import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const initialState = {
+  books: [
+    { id: nanoid(), title: 'Rich Dad Poor Dad', category: 'Finance' },
+    { id: nanoid(), title: 'The Art of seduction', category: 'Non-fiction' },
+  ],
+};
+
+const store = createStore(rootReducer, initialState);
 
 ReactDOM.render(
   <Provider store={store}>
