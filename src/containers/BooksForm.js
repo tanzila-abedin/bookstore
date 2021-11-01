24e8d8274c01d2687ffdcf426156b40a0803bb85
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-11-01 16:06:02 +0200

    Fix options prop name inside select for the options to display

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,19 +1,18 @@
 import Select from 'react-select';
 
-const BooksForm = () => {
-  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  const option = categories.map((category) => ({
-    value: category,
-    label: category,
-  }));
-  return (
-    <div className="booklist">
-      <form>
-        <input type="text" id="title" name="title" placeholder="Book Title" />
-        <Select option={option} />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
-};
+const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+const option = categories.map((category) => ({
+  value: category,
+  label: category,
+}));
+
+const BooksForm = () => (
+  <div className="booklist">
+    <form>
+      <input type="text" id="title" name="title" placeholder="Book Title" />
+      <Select options={option} />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+);
 export default BooksForm;
