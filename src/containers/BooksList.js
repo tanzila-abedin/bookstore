941c38c884c9964761d63c4963d3f4440eb5f123
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-11-02 00:44:21 +0200

    Create handle remove book method to delete book from redux store

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from '../components/Book';
+import { removeBook } from '../actions';
 
 const BooksList = () => {
   const { books } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const handleRemoveBook = (book) => {
+    dispatch(removeBook(book));
+  };
+
   const displayBook = books.map((book) => (
-    <Book key={book.id} book={book} />
+    <Book key={book.id} book={book} removeBook={handleRemoveBook} />
   ));
 
   return (
@@ -16,6 +23,7 @@ const BooksList = () => {
             <th className="booklist-header">ID</th>
             <th className="booklist-title">Title Of Book</th>
             <th className="booklist-category">Category Of Book</th>
+            <th className="booklist-remove">Remove</th>
           </tr>
         </thead>
         <tbody>
