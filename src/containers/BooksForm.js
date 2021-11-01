8b8372e4dd9897cfbc5fe2d080dad737cfe417fb
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-11-01 23:52:52 +0200

    Import use state to display newly added books from the redux store a

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,4 +1,8 @@
 import Select from 'react-select';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { nanoid } from 'nanoid';
+import { createBook } from '../actions';
 
 const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 const option = categories.map((category) => ({
@@ -6,13 +10,30 @@ const option = categories.map((category) => ({
   label: category,
 }));
 
-const BooksForm = () => (
-  <div className="booklist">
-    <form>
-      <input type="text" id="title" name="title" placeholder="Book Title" />
-      <Select options={option} />
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-);
+const BooksForm = () => {
+  const [title, setTitle] = useState();
+  const [category, setCategory] = useState();
+  const dispatch = useDispatch();
+  const id = nanoid();
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    dispatch(createBook({
+      id,
+      title,
+      category,
+    }));
+    e.target.reset();
+  };
+
+  return (
+    <div className="booklist">
+      <form onSubmit={(e) => handleChange(e)}>
+        <input type="text" id="title" name="title" placeholder="Book Title" onChange={(e) => setTitle(e.target.value)} />
+        <Select options={option} onChange={(e) => setCategory(e.value)} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 export default BooksForm;
