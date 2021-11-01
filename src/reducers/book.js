f33288ed5842fc574242508c29407df63597621a
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-11-01 16:02:48 +0200

    Set bookreducer initial state to empty array

--- src/reducers/book.js
+++ src/reducers/book.js
@@ -1,12 +1,6 @@
-// use combire reduce
-import { nanoid } from 'nanoid';
 import { CREATE_BOOK, REMOVE_BOOK } from '../actions';
 
-const initialState = [
-  { id: nanoid(), title: 'Rich Dad Poor Dad', category: 'Finance' },
-  { id: nanoid(), title: 'The Art of seduction', category: 'Non-fiction' },
-];
-const bookReducer = (state = initialState, action) => {
+const bookReducer = (state = [], action) => {
   switch (action.type) {
     case CREATE_BOOK:
 
